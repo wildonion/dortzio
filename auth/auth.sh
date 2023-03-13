ba4f5ffc1426b02050b9a93ffb537c0e44e7811c
dewo <marefimoghaddam@unixerr.com>
2023-03-13 10:47:40 +0100

    change user

--- auth/auth.sh
+++ auth/auth.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-source /home/wildonion/dortzio/dortzioenv/bin/activate 
-cd /home/wildonion/dortzio/auth
+source /home/$USER/dortzio/dortzioenv/bin/activate 
+cd /home/$USER/dortzio/auth
 python manage.py runserver 0.0.0.0:3434
